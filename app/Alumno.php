0555a68df8663b483dba6997145de860be35a346
marcelus33 <marcelob_33@hotmail.com>
2019-10-28 18:55:01 -0300

    Add changes in Alumno and create Padre model

--- app/Alumno.php
+++ app/Alumno.php
@@ -3,11 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Padre;
 
 class Alumno extends Model
 {
     protected $primaryKey = 'id_alumno';
     protected $table = 'alumnos';
 
+    protected $guarded = ['id_alumno'];
+    public $timestamps = false;
+
+    public function padres()
+    {
+       return $this->belongsTo(Padre::class, 'id_padres');
+    }
 
 }
